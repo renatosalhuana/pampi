423228fedcdfd47eab8ff6a77cb9b77a73e9dc67
renatosalhuana <rsalhuana@outlook.com>
2024-11-09 16:13:51 +0100

    php code!

--- barras/index.php
+++ barras/index.php
@@ -1,73 +1,9 @@
-<!DOCTYPE html>
-<html lang="">
+<?php
+$active_menu = "barras";
 
-<head>
-  <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-CJCVGND11N"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
-    gtag('config', 'G-CJCVGND11N');
-  </script>
-  <title>BARRAS - PamPi Mobiliario </title>
-  <link rel="apple-touch-icon" sizes="57x57" href="../webicon/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="../webicon/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="../webicon/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="../webicon/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="../webicon/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="../webicon/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="../webicon/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="../webicon/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="../webicon/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192" href="../webicon/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="../webicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="../webicon/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="../webicon/favicon-16x16.png">
-  <link rel="manifest" href="../webicon/manifest.json">
-  <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="../webicon/ms-icon-144x144.png">
-  <meta name="theme-color" content="#ffffff">
+include_once '../template/header.php';
+?>
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <link href="../layout/styles/layout.css?p=0" rel="stylesheet" type="text/css" media="all">
-  <link href="../layout/styles/mobile.css?p=1" rel="stylesheet" media="screen and (max-width: 470px)" />
-  <link href="../layout/styles/desktop.css?p=1" rel="stylesheet" media="screen and (min-width: 800px)" />
-  <link href="../layout/styles/menu.css?p=0" rel="stylesheet">
-  <link href="../layout/styles/slider.css?p=1" rel="stylesheet" type="text/css" media="all">
-  <script src="../layout/scripts/slider.js"></script>
-  <script src="../layout/scripts/whatsapp.js"></script>
-</head>
-
-<body id="top">
-  <section class="top-nav">
-    <div>
-      <!-- no borrar este div -->
-    </div>
-    <input id="menu-toggle" type="checkbox" />
-    <label class='menu-button-container' for="menu-toggle">
-      <div class='menu-button'></div>
-    </label>
-    <ul class="menu">
-      <li><a href="/">INICIO</a></li>
-      <li class="active"><a href="/barras/">BARRAS</a></li>
-      <li><a href="/mesas-sillas-altas/">MESAS & SILLAS ALTAS</a></li>
-      <li><a href="/salas-lounge/">SALAS LOUNGE</a></li>
-      <li><a href="/estructuras/">ESTRUCTURAS</a></li>
-      <li><a name="whatsapp-link" target="_blank" href="#">CONTACTO</a></li>
-    </ul>
-  </section>
-
-  <div class="wrapper row1">
-    <header id="header" class="hoc clear">
-      <h1>
-        <a href="/">
-          <img style="height:248px" src="../images/Logo PamPi.jpg" alt="logo PamPi">
-        </a>
-      </h1>
-    </header>
-  </div>
   <div class="wrapper row3">
     <main class="hoc container clear">
       <!-- main body -->
