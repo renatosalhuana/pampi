8832feab6c7bbda64a8f7aee6ab7e90f23f59e1b
renatosalhuana <rsalhuana@outlook.com>
2024-12-02 17:12:43 +0100

    footer updates

---
+++ template/footer.php
@@ -0,0 +1,79 @@
+<div class="wrapper row2">
+    <article class="hoc container cta clear" style="font-family: Miracle; font-size: 18px;">
+      <!-- ################################################################################################ -->
+      <h3 class="heading" style="font-family: Sephir; font-size: 35px;">Don't Settle for Boring!</h3>
+      <p style="font-family: Miracle;">Nuestro catálogo está en constante crecimiento<br></br> ¡En PamPi no paramos de diseñar!</p>
+      <footer><a class="btn" href="../CatalogoPamPimobiliario.pdf" target="_blank">Descarga nuestro catálogo</a>
+      </footer>
+      <!-- ################################################################################################ -->
+    </article>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ######################¡¡¡FOOTER!!!########################################################################## -->
+  <div class="wrapper row4">
+    <footer id="footer" class="hoc clear">
+      <!-- ################################################################################################ -->
+      <div class="one_third first">
+        <div style="text-align: center;">
+          <img src="../images/LOGO PAMPI png.png">
+        </div>
+      </div>
+      <div class="one_third">
+        <ul class="nospace btmspace-30 linklist contact">
+          <li><i class="fa fa-map-marker"></i>
+            <address>
+              Lima, Perú
+            </address>
+          </li>
+          <li><i class="fa fa-phone"></i> +51 943986612</li>
+          <li><i class="fa fa-envelope-o"></i> pampi.mobiliario@gmail.com</li>
+        </ul>
+
+      </div>
+      <div class="one_third">
+        <ul class="faico clear">
+          <li>
+            <a class="faicon-facebook" href="https://www.facebook.com/profile.php?id=61556437365099"><i
+                class="fa fa-facebook"></i></a>
+          </li>
+
+          <li><a class="faicon-instagram" href="https://www.instagram.com/pampimobiliario/"><i
+                class="fa fa-instagram"></i></a></li>
+          <li><a name="whatsapp-link" target="_blank" class="faicon-whatsapp" href="#"><i
+                class="fa fa-whatsapp"></i></a>
+          </li>
+        </ul>
+      </div>
+      <!-- ################################################################################################ -->
+    </footer>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div class="wrapper row5">
+    <div id="copyright" class="hoc clear">
+      <!-- ################################################################################################ -->
+      <p class="last-line">Copyright &copy; 2024 - Todos los derechos
+        reservados - <a href="#">PamPi Mobiliario</a> -
+        Desarrollado por <a href="https://miramiweb.es/" target="_blank">Mira mi web</a>
+      </p><!-- ################################################################################################ -->
+    </div>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- <a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a> -->
+  <a href="#" name="whatsapp-link" class="wpfloat" target="_blank">
+    <i class="fa fa-whatsapp mywp-float"></i>
+  </a>
+  <!-- JAVASCRIPTS -->
+  <script src="../layout/scripts/jquery.min.js"></script>
+  <script src="../layout/scripts/jquery.backtotop.js"></script>
+  <script src="../layout/scripts/jquery.mobilemenu.js"></script>
+</body>
+
+</html>
